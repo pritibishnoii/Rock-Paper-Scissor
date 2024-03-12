82d9554311d8707a6cefa76aa294d15468e0a9ad
pritibishnoi <bishnoipriti31@gmail.com>
2024-03-12 18:03:06 +0530

    page2

--- js/script.js
+++ js/script.js
@@ -19,3 +19,39 @@
 //     document.getElementById('result').innerText = "Your choice: " + playerChoice + "\nComputer's choice: " + computerChoice + "\n" + result;
 //   }
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const rulesBtn = document.getElementById('rules-btn');
+
+const closeButton = document.getElementById('cross-circl');
+const gameRules = document.getElementById('rulesBox')
+
+const nextBtn = document.getElementById('next-btn')
+rulesBtn.addEventListener('click' , ()=>{
+    console.log("clicked...")
+    gameRules.style.display='block'; 
+})
+
+closeButton.addEventListener('click' ,(e)=>{
+    gameRules.style.display = 'none'
+})
+
+
+
+
+
