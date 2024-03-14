4d4cb2dde101dd3cd530f4c9c79c7150b1c0071a
pritibishnoi <bishnoipriti31@gmail.com>
2024-03-14 21:25:07 +0530

    adding animation

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-// function play(playerChoice) {
-//     var choices = ['rock', 'paper', 'scissors'];
-//     var computerChoice = choices[Math.floor(Math.random() * choices.length)];
-//     console.log(computerChoice,"computer choosed")
+function play(playerChoice) {
+    var choices = ['rock', 'paper', 'scissors'];
+    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(computerChoice,"computer choosed")
   
-//     var result;
-//     if (playerChoice === computerChoice) {
-//       result = "It's a tie!";
-//     } else if (
-//       (playerChoice === 'rock' && computerChoice === 'scissors') ||
-//       (playerChoice === 'paper' && computerChoice === 'rock') ||
-//       (playerChoice === 'scissors' && computerChoice === 'paper')
-//     ) {
-//       result = "You win!";
-//     } else {
-//       result = "Computer wins!";
-//     }
+    var result;
+    if (playerChoice === computerChoice) {
+      result = "It's a tie!";
+    } else if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      result = "You win!";
+    } else {
+      result = "Computer wins!";
+    }
   
-//     document.getElementById('result').innerText = "Your choice: " + playerChoice + "\nComputer's choice: " + computerChoice + "\n" + result;
-//   }
+    document.getElementById('result').innerText = "Your choice: " + playerChoice + "\nComputer's choice: " + computerChoice + "\n" + result;
+  }
   
 
 
